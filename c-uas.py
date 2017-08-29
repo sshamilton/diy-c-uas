9dd14e5dca6462ef95d9eb6b977b313f5327ae0b
Roy Ragsdale <roy@ragsdale.xyz>
2017-08-29 16:23:06 -0400

    Add function skeletons and comments

--- c-uas.py
+++ c-uas.py
@@ -1,7 +1,59 @@
 #!/usr/bin/python3
 
 
+def init():
+    """
+    Initialization: checks for the presence of necessary tools and configures
+    the logging environment.
+    """
+    pass
+
+
+def scan():
+    """
+    Scan: Runs airodump-ng to characterize the environment.
+    Returns: [string] containing the detected MAC addresses
+    """
+    pass
+
+
+def select_targets():
+    """
+    Target Selection: Compares a list of observed MAC addresses against MAC
+    addresses identified as belonging to sUAS systems.
+    Input: [string] containing MAC addresses from scan
+    Returns: [string] containing target MAC addresses
+    """
+    pass
+
+
+def refine_targets():
+    """
+    Target Refinement: Performs additional scans to gain the required
+    information necessary to perform the attack such as the active channel.
+    Input: [string] containing observed target MAC addresses
+    Returns: [(string,int)] with the (MAC,channel) of each target
+    """
+    pass
+
+
+def deauth_targets():
+    """
+    Detauth Targets: Performs a replay attack against the listed targets
+    Input: [(string,int)] with the (MAC,channel) to target
+    """
+    pass
+
+
 def main():
+    """
+    Main entry point for the script, orchestrates the following actions.
+    1. Initialization - check tools and environment
+    2. Initial scan - characterize wifi-environment
+    3. Target selection - identify MACs associated with sUAS
+    4. Target refinement - gain additional target information (channel)
+    5. DeAuth Targets
+    """
     print("hello")
 
 
